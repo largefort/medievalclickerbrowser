a4ab0b9fc2e4c61c43ad8cff73874434dd3e7a53
jafetegill17 <106451813+largefort@users.noreply.github.com>
2023-12-07 16:35:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let paladinCount = 0;
 let passiveIncome = 0;
 let db;
 let lastSaveTime = Date.now(); // Initialize lastSaveTime with the current time
+let gameIsActive = true; // Indicates if the game is currently active
 
 // Add an HTML audio element for the upgrade sound
 document.write(`
@@ -125,7 +126,6 @@ function requestFullscreen(element) {
     }
 }
 
-
 function updateUI() {
     document.getElementById("counter").textContent = `Gold coins: ${compactNumberFormat(coins)}`;
     document.getElementById("knight-count").textContent = knightCount;
@@ -228,11 +228,48 @@ function earnPassiveIncome() {
     const timeDifference = currentTime - lastSaveTime;
     const offlinePassiveIncome = Math.floor(passiveIncome * (timeDifference / 1000));
 
-    coins += offlinePassiveIncome;
+    if (gameIsActive) {
+        coins += offlinePassiveIncome;
+    }
+
     lastSaveTime = currentTime; // Update the last save time
 
     saveGameData();
     updateUI();
 }
 
-setInterval(earnPassiveIncome, 1000);
+function openOfflineModal() {
+    const modal = document.getElementById("offlineModal");
+    modal.style.display = "block";
+    gameIsActive = false; // Set game as inactive when opening the modal
+    calculateOfflineProgress();
+}
+
+function closeOfflineModal() {
+    const modal = document.getElementById("offlineModal");
+    modal.style.display = "none";
+    gameIsActive = true; // Set game as active when closing the modal
+}
+
+function calculateOfflineProgress() {
+    const lastOnline = localStorage.getItem('lastOnline');
+    if (lastOnline) {
+        const currentTime = Date.now();
+        const offlineTime = currentTime - parseInt(lastOnline);
+        const offlineHours = Math.floor(offlineTime / (1000 * 60 * 60));
+        const offlineMinutes = Math.floor((offlineTime % (1000 * 60 * 60)) / (1000 * 60));
+
+        const offlineEarnings = offlineHours * 10 + offlineMinutes * 0.2;
+
+        document.getElementById("offline-time").textContent = `You were offline for: ${offlineHours} hours ${offlineMinutes} minutes`;
+        document.getElementById("offline-earnings").textContent = `Earnings during this time: ${offlineEarnings.toFixed(2)} coins`;
+    }
+}
+
+const lastOnline = localStorage.getItem('lastOnline');
+if (lastOnline) {
+    openOfflineModal();
+    calculateOfflineProgress();
+}
+
+window.addEventListener('beforeunload', saveLastOnlineTime);
